2625ff55db010d3f56386964a01ed86fc7123b16
aronjarel <aron.jarel@gmail.com>
2024-10-05 15:35:12 -0400

    Added Environment Variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+// For authenticated requests, you can attach the token to headers
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // Attach JWT token
+  }
+  return config;
+});
+
 
 export default apiClient;
